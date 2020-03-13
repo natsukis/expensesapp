8f6e5eddef1d98873ae02d42b135cebf421442c1
natsuki <benja.natsu@gmail.com>
2020-03-12 22:04:34 -0300

    New android icon, app name and splash screen

--- lib/screens/others/initialpage.dart
+++ lib/screens/others/initialpage.dart
@@ -21,30 +21,34 @@ class _SplashScreenState extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Container(child:Center(
-          child: Container(
-              child: Column(children: <Widget>[
-        Padding(
-            padding: EdgeInsets.only(top: 40, left: 20, right: 20),
-            child: Text("GATO: La app de control de vida",
-                style: TextStyle(
-                  fontSize: 25,
-                  foreground: Paint()
-                    ..style = PaintingStyle.stroke
-                    ..strokeWidth = 6
-                    ..color = Colors.blue[300],
-                ))),
-        Padding(
+      body: Center(
+          child: Container(              
+        child:Padding(
             padding: EdgeInsets.only(top: 40, left: 40),
-            child: Text("By Natsuki",
-                style: TextStyle(
-                  fontSize: 20,
-                  foreground: Paint()
-                    ..style = PaintingStyle.stroke
-                    ..strokeWidth = 6
-                    ..color = Colors.blue[400],
-                )))
-      ]))),
-    ));
+            child: Stack(
+  children: <Widget>[
+    // Stroked text as border.
+    Text(
+      'By Natsuki',
+      style: TextStyle(
+        fontSize: 40,
+        foreground: Paint()
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 6
+          ..color = Colors.blue[700],
+      ),
+    ),
+    // Solid text as fill.
+    Text(
+      'By Natsuki',
+      style: TextStyle(
+        fontSize: 40,
+        color: Colors.grey[300],
+      ),
+    ),
+  ],
+))
+      )),
+    );
   }
 }
